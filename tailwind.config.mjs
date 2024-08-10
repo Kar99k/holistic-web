3e7377813763fd464b4079bcc4b12fcea3f0f4ec
kar99k <krthkeyanlee@gmail.com>
2024-08-10 20:21:03 +0530

    feat(setup): initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#564AA3',
+        primary15: 'rgb(86, 74, 163,0.15)',
+        secondary: '#D8CCE9',
+        accent: '#3C1C55',
+        textColor: '#4E3876',
+        textColor80: 'rgba(77, 55, 118, 0.8)',
+        background: '#FEFDFE'
+      }
+    }
+  },
+  darkMode: 'class',
+  plugins: []
+}
