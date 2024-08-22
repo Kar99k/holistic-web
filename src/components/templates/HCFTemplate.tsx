39fa68d5cd4c810c687290e8e80d9072b9d4cb65
kar99k <krthkeyanlee@gmail.com>
2024-08-23 00:21:06 +0530

    feat(ui): created HCF Component

--- src/components/templates/HCFTemplate.tsx
+++ src/components/templates/HCFTemplate.tsx
@@ -1,28 +1,17 @@
-import AboutMe from 'components/organisms/AboutMe'
-import ContactUs from 'components/organisms/ContactUs'
-import FAQ from 'components/organisms/FAQ'
 import Footer from 'components/organisms/Footer'
-import HeroSection from 'components/organisms/HeroSection'
-import HowItWorks from 'components/organisms/HowItWorks'
 import NavbarComponent from 'components/organisms/NavbarComponent'
-import Services from 'components/organisms/Services'
-import Stats from 'components/organisms/Stats'
+import { ReactNode } from 'react'
 
-export default function HCFTemplate() {
+interface HCFTemplateProps {
+  children: ReactNode // Define type for children
+}
+
+export default function HCFTemplate({ children }: HCFTemplateProps) {
   return (
     <div className="h-screen w-full">
       <NavbarComponent />
       <div className="mx-auto flex h-fit flex-col items-center gap-[82px]">
-        <div className="flex flex-col justify-between gap-8 sm:gap-16 lg:gap-24 2xl:gap-32">
-          <HeroSection />
-          <Stats />
-        </div>
-
-        <AboutMe />
-        <Services />
-        <HowItWorks />
-        <ContactUs />
-        <FAQ />
+        {children}
       </div>
       <Footer />
     </div>
